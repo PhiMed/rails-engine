7164e6809feda22820e1c57045f8fee6f3863752
Philip Medlin <phmedlin@gmail.com>
2021-12-06 19:13:24 -0700

    Items feats and tests

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:id])
+    merchant = Merchant.find(params[:merchant_id])
     render json: merchant.items
   end
+
+  # def show
+  #   merchant = Merchant.find(params[:merchant_id])
+  #   item = merchant.items.where(id: params[:id])
+  #   render json: item
+  # end
 end
---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+describe 'Items API' do
+  it 'sends a list of all items' do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    create_list(:item, 3, merchant: merchant_1)
+    create_list(:item, 3, merchant: merchant_2)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq 6
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_a(Integer)
+    end
+
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to eq(id)
+    expect(item[:id]).to be_an(Integer)
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_a(Float)
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_a(Integer)
+  end
+
+  it "can create a new item" do
+      merchant_1 = create(:merchant)
+      item_params = ({
+                      name: 'Shiny New Thing',
+                      description: '25mmx25mm shiny thing',
+                      unit_price: '1000',
+                      merchant_id: merchant_1.id
+                    })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+      created_item = Item.last
+
+      expect(response).to be_successful
+      expect(created_item.name).to eq(item_params[:name])
+
+    end
+
+    it "can update an existing item" do
+      id = create(:item).id
+      previous_name = Item.last.name
+      item_params = { name: "Even Shinier This Time" }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+      item = Item.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(item.name).to_not eq(previous_name)
+      expect(item.name).to eq("Even Shinier This Time")
+    end
+
+    it "can destroy an item" do
+      item = create(:item)
+
+      expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+
+      expect(response).to be_successful
+      expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+  end
